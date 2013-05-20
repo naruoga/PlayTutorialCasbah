8f662695f522b4274bd20e2b9340e74c155641e6
Naruhiko Ogasawara <ogasawara.naruhiko@miraitsystems.jp>
2013-05-20 18:28:29 +0900

    app/models/Task.scala - add procedure to close connection if each queries/modifies are finished

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -2,32 +2,50 @@ package models
 
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.MongoClient
+import com.typesafe.config._
 
 case class Task(id: String, label: String)
 
 object Task {
-	def all() : List[Task] = {
-			(for (x <- connectMongo("playTodo", "TODO").find()) yield {
-				Task(x("_id").toString, x("label").toString)
-			}).toList
-	}
-	def create(label: String) {
-			val col = connectMongo("playTodo", "TODO")
-			val newObj = MongoDBObject("label" -> label)
-			col += newObj
-			for (added <- col.findOne(newObj)) {
-				
-			}
-	}
+  private val appConf = ConfigFactory.load()
+  private val server = appConf.getString("mongodb.server")
+  private val port   = appConf.getInt("mongodb.port")
+  
+  def all() : List[Task] = {
+    println("appllication.conf/mongodb.server=" + server)
+    println("appllication.conf/mongodb.port=" + port.toString)
+    val connect = connectMongo()
+    val col = connect("playTodo")("TODO")
+    try {
+      (for (x <- col.find()) yield {
+	Task(x("_id").toString, x("label").toString)
+      }).toList
+    } finally {
+      connect.close()
+    }
+  }
+
+  def create(label: String) {
+    val connect = connectMongo()
+    val col = connect("playTodo")("TODO")
+    try {
+      val newObj = MongoDBObject("label" -> label)
+      col += newObj
+    } finally {
+      connect.close()
+    }
+  }
 	
-	def delete(id: String) {
-			val col = connectMongo("playTodo", "TODO")
-			val query = MongoDBObject("_id" -> new ObjectId(id))
-			col.remove(query)
-	  
-	}
+    def delete(id: String) {
+      val connect = connectMongo()
+      val col = connect("playTodo")("TODO")
+      try {
+	val query = MongoDBObject("_id" -> new ObjectId(id))
+	col.remove(query)
+      } finally {
+	connect.close()
+      }
+    }
 	
-	def connectMongo(db: String, collection: String) : MongoCollection = {
-			MongoClient()(db)(collection)
-	}
+    def connectMongo() : MongoClient = MongoClient(server, port)
 }
