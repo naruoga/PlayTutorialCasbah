e74c143e856cc2e21acff9932ce86014873bbbc9
Naruhiko Ogasawara <ogasawara.naruhiko@miraitsystems.jp>
2013-05-21 18:25:55 +0900

    app/models/Task.scala - modify configuration notation of replicaSet servers

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -7,10 +7,6 @@ import com.typesafe.config._
 case class Task(id: String, label: String)
 
 object Task {
-  private val appConf = ConfigFactory.load()
-  private val server = appConf.getString("mongodb.server")
-  private val port   = appConf.getInt("mongodb.port")
-  
   def all() : List[Task] = {
     println("appllication.conf/mongodb.server=" + server)
     println("appllication.conf/mongodb.port=" + port.toString)
@@ -47,5 +43,28 @@ object Task {
       }
     }
 	
-    def connectMongo() : MongoClient = MongoClient(server, port)
+    def connectMongo() : MongoClient = {
+      val appConf = ConfigFactory.load()
+      // TODO: make me simpler; just making
+      // "aaa:47017,bbb, ccc" -> (("aaa", 47017), ("bbb", 27017), ("ccc", 27017))
+      // 1) split each server by "," 2) remove space 3) create pair of server and port
+      // (note: if port aren't specified, port should be 27017 as MongoDB normal port)
+      // port number can be Int or String (if Int choosed, replSetSeeds process
+      // should also changed).
+      //
+      val serverInfoStr = appConf.getString("mongodb.servers")
+      val serverInfo = serverInfoStr.split(',') 
+			  map (_.trim)
+			  map ((z: String) => 
+			         { if (z.contains(':')) z.split(':') 
+				   else (z + ":27017").split(':') }
+			      )
+      try {
+	val replSetSeeds = for (Array(svr, port) <- serverInfo) yield new ServerAddress(svr, port.toInt)
+      } catch {
+	ex: MongoException: throw new RuntimeExcepction("can't connect mongo server: " + serverInfoStr)
+	ex: java.net.UnknownHostException: throw new RuntimeException("some server might not collect: " 
+					      + serverInfoStr)
+      }
+      MongoClient(replSetSeeds)
 }
