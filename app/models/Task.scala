7a236a4297eb498a22010d7b39952b6589cbee28
Naruhiko Ogasawara <ogasawara.naruhiko@miraitsystems.jp>
2013-05-21 18:46:08 +0900

    app/models/Task.scala - fix missing parensis in method connectMongo

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -7,6 +7,8 @@ import com.typesafe.config._
 case class Task(id: String, label: String)
 
 object Task {
+  val appConf = ConfigFactory.load()
+
   def all() : List[Task] = {
     println("appllication.conf/mongodb.server=" + server)
     println("appllication.conf/mongodb.port=" + port.toString)
@@ -32,33 +34,31 @@ object Task {
     }
   }
 	
-    def delete(id: String) {
-      val connect = connectMongo()
-      val col = connect("playTodo")("TODO")
-      try {
-	val query = MongoDBObject("_id" -> new ObjectId(id))
-	col.remove(query)
-      } finally {
-	connect.close()
-      }
+  def delete(id: String) {
+    val connect = connectMongo()
+    val col = connect("playTodo")("TODO")
+    try {
+      val query = MongoDBObject("_id" -> new ObjectId(id))
+      col.remove(query)
+    } finally {
+      connect.close()
     }
-	
-    def connectMongo() : MongoClient = {
-      val appConf = ConfigFactory.load()
-      // TODO: make me simpler; just making
-      // "aaa:47017,bbb, ccc" -> (("aaa", 47017), ("bbb", 27017), ("ccc", 27017))
-      // 1) split each server by "," 2) remove space 3) create pair of server and port
-      // (note: if port aren't specified, port should be 27017 as MongoDB normal port)
-      // port number can be Int or String (if Int choosed, replSetSeeds process
-      // should also changed).
-      //
-      val serverInfoStr = appConf.getString("mongodb.servers")
-      val serverInfo = serverInfoStr.split(',') 
-			  map (_.trim)
-			  map ((z: String) => 
-			         { if (z.contains(':')) z.split(':') 
-				   else (z + ":27017").split(':') }
-			      )
+  }
+  
+  def connectMongo() : MongoClient = {
+    // TODO: make me simpler; just making
+    // "aaa:47017,bbb, ccc" -> (("aaa", 47017), ("bbb", 27017), ("ccc", 27017))
+    // 1) split each server by "," 2) remove space 3) create pair of server and port
+    //
+    // (note: if port aren't specified, port should be 27017 as MongoDB normal port)
+    // port number can be Int or String (if Int choosed, replSetSeeds process
+    // should also changed).
+    //
+    val serverInfoStr = appConf.getString("mongodb.servers")
+    val serverInfo = 
+      serverInfoStr.split(',') map (_.trim) map ((z: String) => 
+						  { if (z.contains(':')) z.split(':') 
+						    else (z + ":27017").split(':') })
       try {
 	val replSetSeeds = for (Array(svr, port) <- serverInfo) yield new ServerAddress(svr, port.toInt)
       } catch {
@@ -67,4 +67,5 @@ object Task {
 					      + serverInfoStr)
       }
       MongoClient(replSetSeeds)
+  }
 }
